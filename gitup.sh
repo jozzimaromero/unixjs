621e314fdb4ac16831deab47fd310dac7637e265
joseluis <joseluis8906@opmbx.org>
2016-08-16 23:21:54 -0500

    user auth

--- gitup.sh
+++ gitup.sh
@@ -2,6 +2,22 @@
 cd ./frontend/server/
 make clean
 cd ../../
+cd ./backend/tmp_files/
+rm *
+cd ../../
+cd ./media/documents/
+rm *
+echo "documents">init.txt
+cd ../music/
+rm *
+echo "music">init.txt
+cd ../pictures/
+rm *
+echo "pictures">init.txt
+cd ../videos/
+rm *
+echo "videos">init.txt
+cd ../../
 git add .
 git commit -m "$1"
 git push origin master
